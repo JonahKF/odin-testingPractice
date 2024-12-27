e678018b3bfc75a00c53a43b282acdd986e49cf7
jonah <jonahkf@gmail.com>
2024-12-27 20:46:39 +0900

    Fix: caesarCipher.js now supports capitalization and non-standard chars

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -4,11 +4,18 @@ export const caesarCipher = (string, key) => {
   let newString = "";
 
   for (let n = 0; n < string.length; n++) {
-    let pos = alphabet.indexOf(string.charAt(n));
-    newString += keyAlphabet.charAt(pos);
+    if (alphabet.indexOf(string.charAt(n).toLowerCase()) > -1) {
+      if (string.charAt(n) === string.charAt(n).toUpperCase()) {
+        let pos = alphabet.indexOf(string.charAt(n).toLowerCase());
+        newString += keyAlphabet.charAt(pos).toUpperCase();
+      } else {
+        let pos = alphabet.indexOf(string.charAt(n));
+        newString += keyAlphabet.charAt(pos);
+      }
+    } else {
+      newString += string.charAt(n);
+    }
   }
 
-  return newString.toUpperCase();
+  return newString;
 };
-
-console.log(caesarCipher("spaghetti", 4));
--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -1,5 +1,8 @@
 import { caesarCipher } from "./caesarCipher";
 
 test("tests all calculator functions", () => {
-  expect(caesarCipher("spaghetti", 4)).toBe("WTEKLIXXM");
+  expect(caesarCipher("spaghetti", 4)).toBe("wteklixxm");
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+  expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+  expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
 });
